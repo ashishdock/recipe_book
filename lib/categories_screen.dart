91c3f0fb0e01ea7c99df76631d2681366d262f7b
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2023-01-17 11:45:56 +0530

    Done till adding fonts and updating yml file.

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -7,14 +7,23 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children:
-          DUMMY_CATEGORIES.map((e) => CategoryItem(e.title, e.color)).toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'DeliMeals',
+        ),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES
+            .map((e) => CategoryItem(e.title, e.color))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
