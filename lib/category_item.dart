c206f923cd106999940db469ca9da185b0cf1fd4
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2023-01-17 15:24:49 +0530

    Routing and Navigation

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,24 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_book/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   // const CategoryItem({ Key? key }) : super(key: key);
+  final String id;
   final String title;
   final Color color;
 
-  CategoryItem(this.title, this.color);
+  CategoryItem(this.id, this.title, this.color);
+
+  void selectedCategory(BuildContext context) {
+    Navigator.of(context)
+        .pushNamed('/category-meals', arguments: {'id': id, 'title': title});
+
+    // (MaterialPageRoute(
+    //     builder: (_) => CategoryMealScreen(id, title, color)));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(title),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [color.withOpacity(.3), color],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () {
+        selectedCategory(context);
+      },
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.title,
         ),
-        borderRadius: BorderRadius.circular(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [color.withOpacity(.3), color],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_book/categories_screen.dart';
+import 'package:recipe_book/category_meals_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,9 +14,28 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              body2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              title: TextStyle(
+                fontSize: 24,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
       ),
-      home: CategoriesScreen(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => CategoriesScreen(),
+        CategoryMealScreen.routeName: (context) => CategoryMealScreen(),
+      },
     );
   }
 }
